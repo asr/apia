90d2f1e55aed33f4399348c4db8bd33c267b6339
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2013-10-27 08:10:03 -0500

    Using ByteString.

--- src/Utils/Directory.hs
+++ src/Utils/Directory.hs
@@ -19,6 +19,8 @@ where
 ------------------------------------------------------------------------------
 -- Haskell imports
 
+import qualified Data.ByteString.Lazy as BL
+
 import Control.Monad ( liftM2 )
 
 ------------------------------------------------------------------------------
@@ -26,9 +28,9 @@ import Control.Monad ( liftM2 )
 -- | Return 'True' if the files are equals, otherwise the function
 -- returns 'False'.
 equalFiles ∷ FilePath → FilePath → IO Bool
-equalFiles f1 f2 = liftM2 (==) (readFile f1) (readFile f2)
+equalFiles f1 f2 = liftM2 (==) (BL.readFile f1) (BL.readFile f2)
 
 -- | Return 'True' if the files are different, otherwise the function
 -- returns 'False'.
 notEqualFiles ∷ FilePath → FilePath → IO Bool
-notEqualFiles f1 f2 = liftM2 (/=) (readFile f1) (readFile f2)
+notEqualFiles f1 f2 = liftM2 (/=) (BL.readFile f1) (BL.readFile f2)

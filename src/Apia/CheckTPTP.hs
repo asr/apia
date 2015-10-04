9ad8b308173e1895881f0db9bb541961506a1b2d
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2015-10-04 18:19:52 -0500

    tptp4X output.
    If tptp4X find a warning then this warning is dumped to stdout.

--- src/Apia/CheckTPTP.hs
+++ src/Apia/CheckTPTP.hs
@@ -10,7 +10,8 @@
 -- Check the generated TPTP file using the tptp4X program.
 -----------------------------------------------------------------------------
 
-{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnicodeSyntax     #-}
 
 module Apia.CheckTPTP ( checkTPTP ) where
 
@@ -22,7 +23,7 @@ import Apia.Utils.Directory ( checkExecutable )
 
 import qualified Apia.Utils.Except as E
 
-import Apia.Utils.PrettyPrint ( (<>), Doc, Pretty(pretty) )
+import Apia.Utils.PrettyPrint ( (<>), Doc, Pretty(pretty), sspaces )
 
 import Control.Monad           ( when )
 import Control.Monad.IO.Class  ( MonadIO(liftIO) )
@@ -41,8 +42,8 @@ checkTPTP file = do
   tptp4XExec ← askTOpt optWithtptp4X
 
   let msgError ∷ Doc
-      msgError = pretty "the " <> pretty tptp4XExec
-                 <> pretty " command from the TPTP library does not exist"
+      msgError = "the " <> pretty tptp4XExec
+                 <> " command from the TPTP library does not exist"
 
   checkExecutable tptp4XExec msgError
 
@@ -52,13 +53,15 @@ checkTPTP file = do
                                      []
   case exitCode of
     ExitFailure _ →
-      E.throwE $ pretty tptp4XExec <> pretty " found an error in the file "
+      E.throwE $ pretty tptp4XExec <> sspaces "found an error in the file"
                  <> pretty file
-                 <> pretty "\nPlease report this as a bug"
+                 <> "\nPlease report this as a bug"
 
     -- TODO (11 December 2012). How add a test case for this case?
     ExitSuccess →
       when ("WARNING" `isInfixOf` out) $
-        E.throwE $ pretty tptp4XExec <> pretty " found a warning in the file "
-                   <> pretty file
-                   <> pretty "\nPlease report this as a bug"
+        E.throwE $ pretty tptp4XExec
+                   <> sspaces "found a warning in the file"
+                   <> pretty file <> ". Please report this as a bug.\n"
+                   <> "\nWarning:\n"
+                   <> pretty out

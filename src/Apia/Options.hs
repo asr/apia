9bc2a2acd8cfbe3a5dbb99dc879bbaf60e171122
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2016-12-29 11:21:15 -0500

    [ help ] Documented the online ATPs.

--- src/Apia/Options.hs
+++ src/Apia/Options.hs
@@ -317,8 +317,10 @@ withZ3Opt name opts = Right opts { optWithZ3 = name }
 -- | Description of the command-line 'Options'.
 options ∷ [OptDescr OM]
 options =
-  [ Option []  ["atp"] (ReqArg atpOpt "NAME") $
-               "Set the ATP (cvc4, e, equinox, ileancop, metis, spass, vampire or z3)\n"
+  [ Option []  ["atp"] (ReqArg atpOpt "[online-]NAME") $
+               "Set the ATP\n"
+               ++ "(offline ATPs: cvc4, e, equinox, ileancop, metis, spass, vampire or z3)\n"
+               ++ "(online ATPs: run the ‘onlineatps --list-atps’ command)\n"
                ++ "(default: e, equinox and vampire)"
   , Option []  ["check"] (NoArg checkOpt) $
                "Check the syntax of the generated TPTP files using the\n"

90c7e2f94f922af76c136b0deccbc207a1bd15bf
Jonathan Prieto-Cubides <prieto.jona@gmail.com>
2017-01-18 14:49:19 -0500

    This closes #83 adding commit info in the version of Apia (#86)
    I used a deprecated constructor `versionTags` (See GHC ticket #2496) but it doesn't seem about our concern at least for now. 
    
    References:
    - https://gist.github.com/hvr/656738
    - https://github.com/jkff/vcs-revision/blob/master/Distribution/VcsRevision/Git.hs
    - https://github.com/acfoltzer/gitrev/blob/master/src/Development/GitRev.hs

--- Setup.hs
+++ Setup.hs
@@ -1,3 +1,79 @@
-import Distribution.Simple
 
-main = defaultMain
+-- | Setup.hs module
+
+{-# OPTIONS_GHC -Wall                           #-}
+{-# OPTIONS_GHC -fno-warn-warnings-deprecations #-}
+{-# LANGUAGE ScopedTypeVariables                #-}
+
+import Control.Exception (IOException, try)
+import Data.Maybe (Maybe (Just, Nothing))
+import Data.Version (Version(versionTags))
+import Distribution.Simple
+  ( confHook
+  , defaultMainWithHooks
+  , simpleUserHooks
+  )
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo)
+import Distribution.Simple.Setup (ConfigFlags)
+import Distribution.Package
+  ( PackageIdentifier
+    ( PackageIdentifier
+    , pkgName
+    , pkgVersion
+    )
+  , PackageName
+    ( PackageName
+    , unPackageName
+    )
+  )
+import Distribution.PackageDescription
+  ( GenericPackageDescription (packageDescription)
+  , HookedBuildInfo
+  , PackageDescription (package)
+  )
+import System.Exit (ExitCode(ExitSuccess))
+import System.Process (readProcessWithExitCode)
+
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks { confHook = cHook }
+
+cHook :: (GenericPackageDescription, HookedBuildInfo)
+      -> ConfigFlags
+      -> IO LocalBuildInfo
+cHook (desc, info) cflags = do
+  let pdesc :: PackageDescription
+      pdesc = packageDescription desc
+
+  finalVersion :: Version <- getVersion $ pkgVersion $ package pdesc
+  let newDesc :: GenericPackageDescription
+      newDesc = desc {
+        packageDescription = pdesc {
+            package = PackageIdentifier {
+                  pkgName = PackageName { unPackageName = "apia" }
+                , pkgVersion = finalVersion }
+            }
+        }
+  confHook simpleUserHooks (newDesc, info) cflags
+
+getVersion :: Version -> IO Version
+getVersion version = do
+  commit :: Maybe String <- commitInfo
+  case commit of
+    Nothing  -> return version
+    Just rev -> return $ gitVersion version rev
+
+tryIO :: IO a -> IO (Either IOException a)
+tryIO = try
+
+commitInfo :: IO (Maybe String)
+commitInfo = do
+  res <- tryIO $ readProcessWithExitCode "git" ["log", "--format=%h", "-n", "1"] ""
+  case res of
+    Right (ExitSuccess, hash, _) -> do
+      (_, _, _) <- readProcessWithExitCode "git" ["diff", "--quiet"] ""
+      return $ Just (init hash)
+    _ -> return Nothing
+
+gitVersion :: Version -> String -> Version
+gitVersion version hash = version { versionTags = [take 7 hash] }

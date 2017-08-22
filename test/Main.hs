801aedc50aa51859b1e614be5fa5943da590782b
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2017-08-22 05:47:49 -0500

    [ test-suite ] Disabled interface test on Travis.

--- test/Main.hs
+++ test/Main.hs
@@ -2,18 +2,33 @@
 
 module Main ( main ) where
 
-import Fail.Errors.Test            ( errorsTests )
-import Succeed.NonConjectures.Test ( allNonConjecturesTests )
+import qualified Fail.Errors.Test            as Errors
+import qualified Succeed.NonConjectures.Test as NonConjectures
+
+import System.Environment ( getEnv )
 
 import Test.Tasty                    ( testGroup, TestTree )
-import Test.Tasty.Silver.Interactive ( defaultMain )
+import Test.Tasty.Silver.Filter      ( RegexFilter )
+import Test.Tasty.Silver.Interactive ( defaultMain1 )
 
 ------------------------------------------------------------------------------
 
 allTests ∷ IO TestTree
 allTests = do
-  allErrorsTests ← errorsTests
-  return $ testGroup "tests" [ allNonConjecturesTests, allErrorsTests ]
+  allErrorsTests ← Errors.errorsTests
+  return $ testGroup "tests"
+    [ NonConjectures.allNonConjecturesTests
+    , allErrorsTests
+    ]
+
+couldBeDisabledTests :: [RegexFilter]
+couldBeDisabledTests = NonConjectures.couldBeDisabledTests
 
 main ∷ IO ()
-main = defaultMain =<< allTests
+main = do
+  user ← getEnv "USER"
+
+  let disabledTests ∷ [RegexFilter]
+      disabledTests = if user == "asr" then [] else couldBeDisabledTests
+
+  defaultMain1 disabledTests =<< allTests
--- test/Succeed/NonConjectures/Test.hs
+++ test/Succeed/NonConjectures/Test.hs
@@ -1,92 +1,107 @@
 {-# LANGUAGE UnicodeSyntax #-}
 
-module Succeed.NonConjectures.Test ( allNonConjecturesTests ) where
+module Succeed.NonConjectures.Test
+  ( allNonConjecturesTests
+  , couldBeDisabledTests
+  ) where
 
 import Utils ( agdaBIN, apiaBIN )
 
 import qualified Data.Text as T
 
-import Test.Tasty        ( testGroup, TestTree )
-import Test.Tasty.Silver ( goldenVsProg )
+import Test.Tasty               ( testGroup, TestTree )
+import Test.Tasty.Silver.Filter ( RegexFilter(RFInclude) )
+import Test.Tasty.Silver        ( goldenVsProg )
+
+------------------------------------------------------------------------------
+-- Auxiliary functions
+
+nonConjecturesPath ∷ String
+nonConjecturesPath = "test/Succeed/NonConjectures/"
+
+helper ∷ String → String → [String] → TestTree
+helper exec name arg = goldenVsProg testName goldenFile exec args T.empty
+  where
+    testName ∷ String
+    testName = nonConjecturesPath ++ name
+
+    goldenFile ∷ String
+    goldenFile = testName ++ ".golden"
+
+    args ∷ [String]
+    args = ("-i" ++ nonConjecturesPath) : arg
+
+------------------------------------------------------------------------------
+-- Tests
+
+dumpTypesOptionTest ∷ TestTree
+dumpTypesOptionTest = helper apiaBIN "dump-types-option"
+  [ "--dump-types"
+  , nonConjecturesPath ++ "Foo.agda"
+  ]
+
+helpOptionTest ∷ TestTree
+helpOptionTest = helper apiaBIN "help-option" ["--help"]
+
+interfaceTest ∷ TestTree
+interfaceTest = helper agdaBIN "interface"
+  [ "-v 0"
+  , "-v"
+  , "main:50"
+  ,  nonConjecturesPath ++ "Foo.agda"
+  ]
+
+issue18Test ∷ TestTree
+issue18Test = helper apiaBIN "issue-18"
+  [ "--dump-types"
+  , nonConjecturesPath ++ "Issue18.agda"
+  ]
+
+onlineATPTest ∷ TestTree
+onlineATPTest = helper apiaBIN "online-atp"
+  [ "--atp=online-e"
+  , nonConjecturesPath ++ "TrivialTheorem.agda"
+  ]
+
+onlyFilesOptionTest ∷ TestTree
+onlyFilesOptionTest = helper apiaBIN "only-files-option"
+  [ "--atp=e"
+  , "--only-files"
+  , nonConjecturesPath ++ "TrivialTheorem.agda"
+  ]
+
+verboseOptionTest ∷ TestTree
+verboseOptionTest = helper apiaBIN "verbose-option"
+  [ "--atp=e"
+  , "--only-files"
+  , "-v1"
+  , nonConjecturesPath ++ "TrivialTheorem.agda"
+  ]
+
+unprovenConjectureNoErrorOptionTest ∷ TestTree
+unprovenConjectureNoErrorOptionTest =
+  helper apiaBIN "unproven-conjecture-no-error-option"
+    [ "--atp=e"
+    , "--unproven-conjecture-no-error"
+    , nonConjecturesPath ++ "NoTheorem.agda"
+    ]
 
 ------------------------------------------------------------------------------
 
+-- Sometimes we need to disable these tests (i.e. on Travis) because
+-- the golden files include full paths.
+couldBeDisabledTests :: [RegexFilter]
+couldBeDisabledTests = [ RFInclude "test/Succeed/NonConjectures/interface" ]
+
 allNonConjecturesTests ∷ TestTree
 allNonConjecturesTests = testGroup "non-conjectures"
-  [ helpOption
-  , onlyFilesOption
-  , verboseOption
-  , unprovenConjectureNoErrorOption
-  , interface
-  , dumpTypesOption
-  , issue18
-  , onlineATP
+  [ dumpTypesOptionTest
+  , helpOptionTest
+  , interfaceTest
+  , issue18Test
+  , onlineATPTest
+  , onlyFilesOptionTest
+  , verboseOptionTest
+  , unprovenConjectureNoErrorOptionTest
   ]
 
-  where
-  nonConjecturesPath ∷ String
-  nonConjecturesPath = "test/Succeed/NonConjectures/"
-
-  helper ∷ String → String → [String] → TestTree
-  helper exec name arg = goldenVsProg testName goldenFile exec args T.empty
-    where
-      testName ∷ String
-      testName = nonConjecturesPath ++ name
-
-      goldenFile ∷ String
-      goldenFile = testName ++ ".golden"
-
-      args ∷ [String]
-      args = ("-i" ++ nonConjecturesPath) : arg
-
-  helpOption ∷ TestTree
-  helpOption = helper apiaBIN "help-option" ["--help"]
-
-  onlyFilesOption ∷ TestTree
-  onlyFilesOption = helper apiaBIN "only-files-option"
-    [ "--atp=e"
-    , "--only-files"
-    , nonConjecturesPath ++ "TrivialTheorem.agda"
-    ]
-
-  verboseOption ∷ TestTree
-  verboseOption = helper apiaBIN "verbose-option"
-    [ "--atp=e"
-    , "--only-files"
-    , "-v1"
-    , nonConjecturesPath ++ "TrivialTheorem.agda"
-    ]
-
-  unprovenConjectureNoErrorOption ∷ TestTree
-  unprovenConjectureNoErrorOption =
-    helper apiaBIN "unproven-conjecture-no-error-option"
-      [ "--atp=e"
-      , "--unproven-conjecture-no-error"
-      , nonConjecturesPath ++ "NoTheorem.agda"
-      ]
-
-  interface ∷ TestTree
-  interface = helper agdaBIN "interface"
-    [ "-v 0"
-    , "-v"
-    , "main:50"
-    ,  nonConjecturesPath ++ "Foo.agda"
-    ]
-
-  dumpTypesOption ∷ TestTree
-  dumpTypesOption = helper apiaBIN "dump-types-option"
-    [ "--dump-types"
-    , nonConjecturesPath ++ "Foo.agda"
-    ]
-
-  issue18 ∷ TestTree
-  issue18 = helper apiaBIN "issue-18"
-    [ "--dump-types"
-    , nonConjecturesPath ++ "Issue18.agda"
-    ]
-
-  onlineATP ∷ TestTree
-  onlineATP = helper apiaBIN "online-atp"
-    [ "--atp=online-e"
-    , nonConjecturesPath ++ "TrivialTheorem.agda"
-    ]
